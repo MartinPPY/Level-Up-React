545db2e6b4a7aa08aba955151cfa94cbe3513b53
MartinPPY <martinsantiago.se@gmail.com>
2026-01-12 17:36:02 -0300

    agregando productos al home

---
+++ assets/scripts.js
@@ -0,0 +1,22 @@
+//SCRIPT PARA COLOCAR PRODUCTOS EN EL HOME
+const renderProducts = () => {
+    const container = document.querySelector("#products > .row")
+
+    for (let i = 0; i <= 2; i++) {
+        container.innerHTML += `
+        <div class="card p-4 col-lg bg-dark text-white border-white">
+            <h5 class="card-title" id="card-title"> ${productos[i].nombre}</h5>
+            <p class="card-text" id="card-text">  ${productos[i].precio} </p>
+            <a href="#" class="btn btn-level-up">Agregar al carrito</a>
+        </div>
+        `
+    }
+
+
+
+
+}
+
+addEventListener("load", () => {
+    renderProducts()
+})
