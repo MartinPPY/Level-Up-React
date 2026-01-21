793785b1aca35bfd747b826c270c9da3ddf25565
MartinPPY <martinsantiago.se@gmail.com>
2026-01-21 15:48:54 -0300

    carrito

--- src/pages/tienda/Tienda.jsx
+++ src/pages/tienda/Tienda.jsx
@@ -4,17 +4,11 @@ import { Registro } from '../tienda/registro/Registro'
 import { Login } from '../tienda/login/Login'
 import { Contacto } from '../tienda/contacto/Contacto'
 import { ProductDetail } from '../tienda/product-detail/ProductDetail'
-<<<<<<< HEAD
-import { Header } from '../../components/header/Header'
-import { Footer } from '../../components/footer/Footer'
-import { Cart } from '../tienda/cart/Cart'
-=======
 import {Header} from '../../components/header/Header'
 import {Footer} from '../../components/footer/Footer'
 import { Nosotros } from "./nosotros/Nosotros"
 import { Blogs } from "./blogs/Blogs"
 
->>>>>>> e3fe2eea909b1a465a50b8277c0808a7cdaf685b
 
 
 export const Tienda = () => {
@@ -30,13 +24,8 @@ export const Tienda = () => {
                         <Route path="/login" element={<Login />} />
                         <Route path="/contacto" element={<Contacto />} />
                         <Route path="/producto-detalle/:codigo" element={<ProductDetail />} />
-<<<<<<< HEAD
-
-                        
-=======
                         <Route path="/nosotros" element={<Nosotros />} />
                         <Route path="/blogs" element={<Blogs />} />
->>>>>>> e3fe2eea909b1a465a50b8277c0808a7cdaf685b
                     </Routes>
                     <Footer />
                 </main>
